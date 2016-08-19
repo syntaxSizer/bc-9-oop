a56ec109c1d5974d33c5515f5a5f5cc29b606694
syntaxSizer <kakuma4never@gmail.com>
2016-08-19 23:24:40 +0300

    edit .gitignore , sum_test.py align docstring

--- TDD/sum/sum_test.py
+++ TDD/sum/sum_test.py
@@ -6,9 +6,9 @@ class MySumTests(unittest.TestCase):
     def setUp(self):
         self.numbers = [10,5,7,8,90,'a','b']
     def	test_sum_of_digits(self):
-        '''
+    '''    
     test sum of digits/number
-        '''
+    '''    
         result = my_sum(5,10)
         self.assertEqual(result,15,'test faild testcase 5 + 10')
         self.assertEqual(my_sum(10,15),25)
